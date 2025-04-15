61fa9c4399c3e756110e4bff1cf7e5e31ddbf215
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-14 21:34:07 -0400

    corregir test

--- myapp/test.py
+++ myapp/test.py
@@ -1,11 +1,17 @@
-from django.test import TestCase, Client
+from datetime import datetime
+from unittest.mock import patch
 
-# Test del endpoint ping
-class PingTests(TestCase):
-    def setUp(self):
-        self.client = Client()
+from django.test import TestCase
 
-    def test_endpoint_ping_responde_ping_pong(self):
-        response = self.client.get('/ping/')
-        self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(response.content, {'ping': 'pong'})
+
+class PingTestCase(TestCase):
+    def test_ping(self):
+        fecha_esperada = "2024-03-20T12:00:00"
+        with patch("myapp.views.datetime") as mock_datetime:
+            mock_datetime.now.return_value = datetime.fromisoformat(fecha_esperada)
+            response = self.client.get("/ping/")
+            self.assertEqual(response.status_code, 200)
+            data = response.json()
+            self.assertEqual(data["ping"], "pong")
+            self.assertIsInstance(data["date"], str)
+            self.assertEqual(data["date"], fecha_esperada)
