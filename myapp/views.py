ef2c401e6099b2f6692016bc7565aa43527d15fe
Pablo <pablo.silvaco@alumnos.usm.cl>
2025-04-14 21:32:13 -0400

    correccion test

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,17 @@
-from django.http import JsonResponse
 from datetime import datetime
+from unittest.mock import patch
 
-def ping(request):
-    return JsonResponse({'ping': 'pong', 'date': datetime.now().isoformat()})
+from django.test import TestCase
+
+
+class PingTestCase(TestCase):
+    def test_ping(self):
+        fecha_esperada = "2024-03-20T12:00:00"
+        with patch("myapp.views.datetime") as mock_datetime:
+            mock_datetime.now.return_value = datetime.fromisoformat(fecha_esperada)
+            response = self.client.get("/ping/")
+            self.assertEqual(response.status_code, 200)
+            data = response.json()
+            self.assertEqual(data["ping"], "pong")
+            self.assertIsInstance(data["date"], str)
+            self.assertEqual(data["date"], fecha_esperada)
